18b33a9b76d1e7181ed15ef7471a57e601a427f2
taltla1028 <75877260+taltla1028@users.noreply.github.com>
2021-04-28 23:54:52 +0900

    비디어 서버업로드

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -3,6 +3,7 @@ import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 import Axios from 'axios';
 import { useSelector} from 'react-redux';
+
 const { TextArea } = Input ;
 const { Title } = Typography;
 
