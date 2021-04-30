a1bc04193643317eb03124bffe94fb096e1813a6
taltla1028 <75877260+taltla1028@users.noreply.github.com>
2021-05-01 03:48:02 +0900

    Landingpage

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,36 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { Card, Avatar, Col, Typography, Row } from 'antd';
-import axios from 'axios';
 import moment from 'moment';
+import Axios from 'axios';
 const { Title } = Typography;
 const { Meta } = Card;
 function LandingPage() {
+
+    const [Video, setVideo] = useState([])
+
+    useEffect(() => {
+
+        Axios.get('/api/video/getVideos')
+        .then(response => {
+            if(response.data.success) {
+                    console.log(response.data)
+                    setVideo(response.data.videos)
+            } else {
+                alert(' 비디오 가져오기를 실패 했습니다. ')
+            }
+        })
+
+    }, [])
+
+    
+    const renderCards = Video.map((video,index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration - minutes * 60 ));
+
+        return <Col  lg={6} md={8} xs={24}>
+                    <a href={`video/post/${video._id}`}>
+                        <div style={{position: `relative` }}>
+                            <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`}  />
+                            
+                                <div className="duration">
+                                    <span>{minutes} : {seconds}</span>
+                                </div>
+                        </div>
+                    </a>
+                    <br />
+                    <Meta
+                        avatar={
+                            <Avatar src={video.writer.image} />
+                        }
+                        title={video.title}
+                        description=""
+                        />
+                        <span>{video.writer.name} </span><br />
+                        <span style={{ marginLeft: '3rem'}}>{video.views} views</span>  - <span>{moment(video.createdAt).format("MMM do YY")}</span>
+                </Col>
+    })
+    
     return (
         <div style={{ width: '85%', margin: '3rem auto'}}>
             <Title level ={2}> Recommended </Title>
             <hr/>
             <Row gutter={[32,16]}>
-                <Col  lg={6} md={8} xs={24}>
-                    {/* <a href={`vide/post/${video_id}`}> */}
-                        <div style={{position: `relative` }}>
-                            {/* <img style={{width: '100%' }} src={`http//localhost:5000/${video.thumbnail}`} alt="thumbnail" /> */}
-                                <div className="duration">
-                                    {/* <span>{minutes} : {seconds}</span> */}
-                                </div>
-                        </div>
-                    {/* </a> */}
-                    <br />
-                    <Meta
-                        // avatar={
-                        //     <Avatar src={video.writer.image} />
-                        // }
-                        // title={video.title}
-                        description=""
-                        />
-                        {/* <span>{video.writer.name} </span><br />
-                        <span style={{ marginLeft: '3rem'}}>{video.views} views</span>  - <span>{moment(video.createdAt).format("MMM do YY")}</span> */}
-                </Col>
+
+                {renderCards}
+                
             </Row>
         </div>
     )
