7caa9049a3c82cd446b3393a32a65998360a967d
taltla1028 <75877260+taltla1028@users.noreply.github.com>
2021-05-10 19:07:16 +0900

    댓글

--- client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import Axios from 'axios'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 
 function Comment() {
+    const videoId = props.match.params.videoId
+    const user = useSelector(state => state.user);
+    const [commentValue, setcommentValue] = useState("")
+    const handleClick = (event) => {
+        setcommentValue(event.currentTarget.value)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const variables = {
+            content: commentValue ,
+            writer: user.userData._id,
+            postId: videoId,
+        }
+
+        Axios.post('/api/comment/saveComment', variables )
+        .then(response => {
+            if(response.data.success ){
+
+            }else{
+                alert(' 커멘트를 저장하지 못했습니다. ')
+            }
+        })
+    }
     return (
         <div>
-            Comment
+            <br />
+            <p> Replies </p>
+            <hr />
+
+            {/* comment Lists  */}
+
+            {/* Root Comments Form */}
+            <form style={{ display:'flex'}} onSubmit={onSubmit} >
+            <textarea
+                style={{ width: '100%', borderRadius: '5px' }}
+                onChange={handleClick} //타이핑 
+                value={commentValue}
+                placeholder="댓글을 작성해 주세요."
+                />
+                <br />
+                <button style={{width:'20%', height:'52px'}} onClick={onSubmit} >Submit</button>
+
+            </form>
         </div>
     )
 }
---
+++ server/routes/comment.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const { Comment } = require("../models/Comment");
+
+
+
+
+//=================================
+//             Comment
+//=================================
+
+
+
+module.exports = router;
