ee532c854aa09e2b7c75663c19f4b1232fb5f4c3
taltla1028 <75877260+taltla1028@users.noreply.github.com>
2021-05-15 13:48:28 +0900

    like,dislike 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,69 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { Card, Avatar, Col, Typography, Row } from 'antd';
-import moment from 'moment';
-import Axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Card, Avatar, Col, Typography, Row } from "antd";
+import moment from "moment";
+import Axios from "axios";
 const { Title } = Typography;
 const { Meta } = Card;
 function LandingPage() {
+  const [Video, setVideo] = useState([]);
 
-    const [Video, setVideo] = useState([])
+  useEffect(() => {
+    Axios.get("/api/video/getVideos").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setVideo(response.data.videos);
+      } else {
+        alert(" 비디오 가져오기를 실패 했습니다. ");
+      }
+    });
+  }, []);
 
-    useEffect(() => {
+  const renderCards = Video.map((video, index) => {
+    var minutes = Math.floor(video.duration / 60);
+    var seconds = Math.floor(video.duration - minutes * 60);
 
-        Axios.get('/api/video/getVideos')
-        .then(response => {
-            if(response.data.success) {
-                    console.log(response.data)
-                    setVideo(response.data.videos)
-            } else {
-                alert(' 비디오 가져오기를 실패 했습니다. ')
-            }
-        })
-
-    }, [])
-
-    
-    const renderCards = Video.map((video,index) => {
-
-        var minutes = Math.floor(video.duration / 60);
-        var seconds = Math.floor((video.duration - minutes * 60 ));
-
-        return <Col  lg={6} md={8} xs={24}>
-                    
-                        <div style={{position: `relative` }}>
-                        <a href={`/video/${video._id}`}>
-                            <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`}  />
-                            
-                                <div className="duration">
-                                    <span>{minutes} : {seconds}</span>
-                                </div>
-                                </a>
-                        </div>
-                    
-                    <br />
-                    <Meta
-                        avatar={
-                            <Avatar src={video.writer.image} />
-                        }
-                        title={video.title}
-                        description=""
-                        />
-                        <span>{video.writer.name} </span><br />
-                        <span style={{ marginLeft: '3rem'}}>{video.views} views</span>  - <span>{moment(video.createdAt).format("MMM do YY")}</span>
-                </Col>
-    })
-    
     return (
-        <div style={{ width: '85%', margin: '3rem auto'}}>
-            <Title level ={2}> Recommended </Title>
-            <hr/>
-            <Row gutter={[32,16]}>
+      <Col lg={6} md={8} xs={24}>
+        <div style={{ position: `relative` }}>
+          <a href={`/video/${video._id}`}>
+            <img
+              style={{ width: "100%" }}
+              alt="thumbnail"
+              src={`http://localhost:5000/${video.thumbnail}`}
+            />
 
-                {renderCards}
-                
-            </Row>
+            <div className="duration">
+              <span>
+                {minutes} : {seconds}
+              </span>
+            </div>
+          </a>
         </div>
-    )
+        <br />
+        <Meta
+          avatar={<Avatar src={video.writer.image} />}
+          title={video.title}
+          description=""
+        />
+        <span>{video.writer.name} </span>
+        <br />
+        <span style={{ marginLeft: "3rem" }}>{video.views} views</span> -{" "}
+        <span>{moment(video.createdAt).format("MMM do YY")}</span>
+      </Col>
+    );
+  });
 
+  return (
+    <div style={{ width: "85%", margin: "3rem auto" }}>
+      <Title level={2}> Recommended </Title>
+      <hr />
+      <Row gutter={[32, 16]}>{renderCards}</Row>
+    </div>
+  );
 }
-export default LandingPage
+export default LandingPage;
---
+++ server/routes/like.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const router = express.Router();
+
+const { Like } = require("../models/Like");
+const { Dislike } = require("../models/Dislike");
+
+//=================================
+//            Like
+//=================================
+
+router.post("/getLikes", (req, res) => {
+  let variable = {};
+
+  if (req.body.videoId) {
+    variable = { videoId: req.body.videoId };
+  } else {
+    variable = { commentId: req.body.commentId };
+  }
+
+  Like.find(variable).exec((err, likes) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({ success: true, likes });
+  });
+});
+
+router.post("/getDislikes", (req, res) => {
+  let variable = {};
+
+  if (req.body.videoId) {
+    variable = { videoId: req.body.videoId };
+  } else {
+    variable = { commentId: req.body.commentId };
+  }
+
+  Dislike.find(variable).exec((err, dislikes) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({ success: true, dislikes });
+  });
+});
+
+router.post("/uplike", (req, res) => {
+  let variable = {};
+
+  if (req.body.videoId) {
+    variable = { videoId: req.body.videoId , userId: req.body.userId };
+  } else {
+    variable = { commentId: req.body.commentId , userId: req.body.userId};
+  }
+
+  // Like collection 에다가 클릭 정보를 넣어 줄거에요.
+
+    const like = new Like(variable)
+
+    like.save((err, likeResult)=>{
+        if(err) return resjson({ success: false, err})
+
+        // 만약에 Dislike 이 이미 클릭이 되었다면, Dislike를 1 줄여준다.
+        Dislike.findByIdAndDelete(variable)
+            .exec((err, disLikeResult) => {
+                if (err) return res.status(400).json({ success: false, err})
+                res.status(200).json({ success: true })
+            })
+    })
+  
+
+
+  
+
+});
+
+router.post("/unLike", (req, res) => {
+    let variable = {};
+  
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId , userId: req.body.userId };
+      } else {
+        variable = { commentId: req.body.commentId , userId: req.body.userId};
+      }
+  
+   
+    Like.findByIdAndDelete(variable)
+        .exec((err, result)=>{
+            if(err) return res.status(400).json({success: false,err})
+            res.status(200).json({success: true })
+        })
+
+
+
+  });
+
+  router.post("/unDislike", (req, res) => {
+    let variable = {};
+  
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId , userId: req.body.userId };
+      } else {
+        variable = { commentId: req.body.commentId , userId: req.body.userId};
+      }
+  
+   
+    Dislike.findByIdAndDelete(variable)
+        .exec((err, result)=>{
+            if(err) return res.status(400).json({success: false,err})
+            res.status(200).json({success: true })
+        })
+
+
+
+  });
+
+  router.post("/upDislike", (req, res) => {
+    let variable = {};
+  
+    if (req.body.videoId) {
+      variable = { videoId: req.body.videoId , userId: req.body.userId };
+    } else {
+      variable = { commentId: req.body.commentId , userId: req.body.userId};
+    }
+  
+   
+   const dislike = new Dislike(variable)
+
+   dislike.save((err, disLikeResult) => {
+       if (err) return resjson({ success: false, err })
+
+       // 만약에 Like 이 이미 클릭이 되었다면, Like 1 줄여준다.
+       Like.findByIdAndDelete(variable)
+       .exec((err, likeResult) => {
+           if(err) return res.status(400).json({success: false, err })
+           res.status(200).json({ success: true })
+       })
+   })
+
+
+
+  });
+
+module.exports = router;
